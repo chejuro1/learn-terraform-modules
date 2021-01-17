1e5436cf33663ec0dea7a829cf2174816cdbfe8e
chejuro1 <46506190+chejuro1@users.noreply.github.com>
2021-01-17 01:12:33 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,3 +48,14 @@ module "ec2_instances" {
     Environment = "dev"
   }
 }
+    
+    module "website_s3_bucket" {
+  source = "./modules/aws-s3-static-website-bucket"
+
+  bucket_name = "module"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
